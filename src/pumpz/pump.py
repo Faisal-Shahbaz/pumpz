84b2c7d5ea285ede75a85e4d9ae10f7cb6bbf3db
Faisal-Shahbaz <faisalsshahbaz1@gmail.com>
2024-10-11 12:59:22 -0400

    v0.1.0a1

--- src/pumpz/pump.py
+++ src/pumpz/pump.py
@@ -12,18 +12,61 @@ class pump:
     ):
         self.file = file
         self.dia = dia
+        if self.dia < 0.1 or self.dia > 50.0:
+            raise Exception('Diameter is invalid. Must be between 0.1 - 50.0 mm')
         self.time = time
         self.loop = []
+        self.rate = 0
+        if self.dia > 14.0:
+            self.vol_units = 'mcL'
+        else:
+            self.vol_units = 'mL'
+        # self.vol_inf=0
+        # self.vol_wdr=0
+        self.dir = ''
+        self.rat = 0
+
 
     def init(*args):
         for self in args:
             self.file.write(f"dia {self.dia}\nal 1\nbp 1\nPF 0\n")
 
-    def rat(self, rate: int, vol: int, dir: str):
-        self.file.write(f"\nphase\nfun rat\nrat {rate} mm\nvol {vol}\ndir {dir}\n")
+    def rate(self, rate: int, vol: int, dir: str):
+        self.file.write(f"\nphase\nfun rat\nrat {self.rat} mm\nvol {vol}\ndir {self.dir}\n")
+        self.dir = dir
+        self.rat = rate
         self.time += vol / rate * 60 * self.getloop()
+    
+    # def fill(fill, rate:int = self.rat, dir = 0):
+    #     self.rat = rate
+    #     self.file.write(f"\nphase\nfun fil\nrat {self.rat} mm\ndir {self.dir}\n")
+    #     if dir == 0:
+    #         if self.dir == 'inf':
+    #             self.dir = 'wdr'
+    #             self.time += self.vol_inf / rate * 60 * self.getloop()
+    #             self.vol_wdr -= 
+    #         elif self.dir == 'wdr':
+    #             self.dir == 'inf'
+    #             self.time += self.vol_wdr / rate * 60 * self.getloop()
+    #         else:
+    #             raise Exception(f'Error in {self}.dir={self.dir}')
+    #     else:
+    #         self.dir = dir
+    #         if self.dir == 'inf':
+    #             self.time += self.vol_inf / rate * 60 * self.getloop()
+    #         elif self.dir == 'wdr':
+    #             self.time += self.vol_wdr / rate * 60 * self.getloop()
+    #         else:
+    #             raise Exception(f'Error in {self}.dir={self.dir}')
 
-    def pas(self, length: int):
+    # increment
+
+    # decrement
+
+    def beep(self):
+        self.file.write(f'\nphase\nfun bep\n')
+
+    def pause(self, length: int):
         if length <= 99:
             self.file.write(f"\nphase\nfun pas {length}\n")
             self.time += length * self.getloop()
