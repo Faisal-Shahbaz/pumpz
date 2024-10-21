5c3523b3c35d22404cabe075975c3fe6573e75a3
Faisal-Shahbaz <faisalsshahbaz1@gmail.com>
2024-10-21 13:40:28 -0400

    0.2.0rc1 and ipzpplwriter corrected

--- src/pumpz/pump.py
+++ src/pumpz/pump.py
@@ -3,116 +3,310 @@ import sympy
 from .utilities import decompose_dict, factor_check
 
 class pump:
-
+    '''
+    For pump objects, one for each pump to be controlled
+    '''
     def __init__(
         self,
         file,
-        dia,
-        time=0,
+        dia: float,
+        rate_units: str = 'mm',
+        vol_units: str = '',
+        time: float = 0,
     ):
+        '''
+        Contains the following attributes of pump() object:
+        file: file object
+        dia: diameter of syringe
+        rate_units: str() of unit for rate. 'mm' for mL/min; 'um' for mcL/min; 'mh' for mL/hr; 'uh' for mcL/h
+        vol_units: str() of unit for volume. 'mcL' or 'mL'. 
+        time: initial time in seconds
+        '''
         self.file = file
+
         self.dia = dia
         if self.dia < 0.1 or self.dia > 50.0:
             raise Exception('Diameter is invalid. Must be between 0.1 - 50.0 mm')
+        
         self.time = time
+
+        self.rate_units = rate_units
+        
+        # self.loop is used to store current loop, used for self.time and loopstart/loopend
         self.loop = []
-        self.rate = 0
-        if self.dia > 14.0:
-            self.vol_units = 'mcL'
+
+        # set self.vol_units depending on user input / lack thereof
+        if vol_units == '':
+            if self.dia > 14.0: 
+                self.vol_units = 'mcL'
+            else:
+                self.vol_units = 'mL'
         else:
-            self.vol_units = 'mL'
-        # self.vol_inf=0
-        # self.vol_wdr=0
-        self.dir = ''
-        self.rat = 0
+            self.vol_units = vol_units
+
+        # Set to 'inf' or 'wdr' for rate()
+        self.__dir = ''
+
+        # Used in rate()
+        self.__rat = 0
+
+        # Used in phase labelling
+        self.phase_name = ''
+
+        # Used to track phases
+        self.phase_num = 1
+
+        # dict of keys phase_name and values phase_number
+        self.phase_ref = {}
+
+        # True if sync is useable, some functions take an unknown time to elapse and are therefore incompatible with sync
+        self.sync_is_useable = True
+
+        # unused
+        # self.sub_program= {}
 
 
     def init(*args):
+        '''
+        Sets PPL to default values. 
+
+        Inputs:
+        ===========================
+        *args: any number of pump objects
+        '''
         for self in args:
             self.file.write(f"dia {self.dia}\nal 1\nbp 1\nPF 0\n")
-
-    def rate(self, rate: int, vol: int, dir: str):
-        self.file.write(f"\nphase\nfun rat\nrat {self.rat} mm\nvol {vol}\ndir {self.dir}\n")
-        self.dir = dir
-        self.rat = rate
-        self.time += vol / rate * 60 * self.getloop()
     
-    # def fill(fill, rate:int = self.rat, dir = 0):
-    #     self.rat = rate
-    #     self.file.write(f"\nphase\nfun fil\nrat {self.rat} mm\ndir {self.dir}\n")
-    #     if dir == 0:
-    #         if self.dir == 'inf':
-    #             self.dir = 'wdr'
-    #             self.time += self.vol_inf / rate * 60 * self.getloop()
-    #             self.vol_wdr -= 
-    #         elif self.dir == 'wdr':
-    #             self.dir == 'inf'
-    #             self.time += self.vol_wdr / rate * 60 * self.getloop()
-    #         else:
-    #             raise Exception(f'Error in {self}.dir={self.dir}')
-    #     else:
-    #         self.dir = dir
-    #         if self.dir == 'inf':
-    #             self.time += self.vol_inf / rate * 60 * self.getloop()
-    #         elif self.dir == 'wdr':
-    #             self.time += self.vol_wdr / rate * 60 * self.getloop()
-    #         else:
-    #             raise Exception(f'Error in {self}.dir={self.dir}')
+    def __phase_to_string(self, phase) -> str:
+        '''
+        Inputs:
+        ============================
+        self: pump object
+        phase: phase number OR phase name ( as set by self.label(label: str) )
 
-    # increment
+        Output:
+        ============================
+        str of phase number in form 'nn' where n is a digit from 0 to 9
+        '''
+        if type(phase) == type(1):
+            if phase > 0 and phase < 99:
+                return str(phase).zfill(2)
+            else:
+                raise Exception(f'phase argument is invalid')
+        elif type(phase) == type('string'):
+            return str(self.phase_ref(self.phase_name)).zfill(2)
+    
+    def __phase(self):
+        '''
+        Writes a phase with current self.phase_name, then empties self.phase_name
+        '''
+        self.file.write(f'\nphase {self.phase_name}\n')
+        self.phase_ref[self.phase_name] = self.phase_num
+        self.phase_name = ''
+        self.phase_num += 1
 
-    # decrement
+    def label(self, label: str) -> str:
+        '''
+        Label the following phase with input label: str, and returns that same str. 
+        '''
+        self.phase_name = label
+        return label
+
+    def change_rate_units(self, rate_units: str):
+        '''
+        Inputs:
+        ==========================
+        rate_units: str() of unit for rate. 'mm' for mL/min; 'um' for mcL/min; 'mh' for mL/hr; 'uh' for mcL/h
+        '''
+        self.rate_units = rate_units
+
+    def rate(self, rate: float, vol: float, dir: str):
+        '''
+        Write PPl function 'rat' (rate)
+
+        Input:
+        =============================
+        self: pump object
+        rate: float or int: rate to infuse/withdraw at current rate units
+        vol: float or int: volume to infuse/withdraw at pump volume units
+        dir: str: either 'inf' for infuse, or 'wdr' for withdraw
+
+        Updates self.time
+        '''
+        self.__phase()
+        self.__dir = dir
+        self.__rat = rate
+
+        self.file.write(f"fun rat\nrat {rate} {self.rate_units}\nvol {vol}\ndir {dir}\n")
+
+        if self.vol_units == 'mcL':
+            v = vol / 1000
+        else:
+            v = vol
+
+        if self.rate_units == 'mm': 
+            self.time += v / rate * 60 * self.getloop()
+        elif self.rate_units == 'um': 
+            self.time += v*1000 / rate * 60 * self.getloop() 
+        elif self.rate_units == 'mh': 
+            self.time += v / rate * 3600 * self.getloop()
+        elif self.rate_units == 'uh': 
+            self.time += v*1000 / rate * 3600 * self.getloop()
+    
+    # fil, incr, decr, not included yet
 
     def beep(self):
-        self.file.write(f'\nphase\nfun bep\n')
+        '''
+        Writes fun bep to PPL
+        '''
+        self.__phase()
+        self.file.write(f'fun bep\n')
 
-    def pause(self, length: int):
+    def pause(self, length: int, phases = 0):
+        '''
+        Automatically writes fun pas with as few phases as possible. Accepts any positive integer for length input (in seconds).
+        '''
         if length <= 99:
-            self.file.write(f"\nphase\nfun pas {length}\n")
+            self.__phase()
+            self.file.write(f"fun pas {length}\n")
             self.time += length * self.getloop()
-
+            phases += 1
         elif length <= 99 * 3:
-            self.pas(99)
-            self.pas(length - 99)
+            phases += self.pause(99)
+            phases += self.pause(length - 99)
         else:
             multiples = factor_check(decompose_dict(sympy.factorint(length)))
             if multiples != (0, 0) and len(multiples) <= 3:
                 for i in range(len(multiples) - 1):
                     self.loopstart(multiples[1 + i])
-                self.pas(multiples[0])
+                    phases += 1
+                self.pause(multiples[0])
                 for i in range(len(multiples) - 1):
                     self.loopend()
+                    phases += 1
             else:
-                self.pas(length % 50, self.getloop())
+                phases += self.pause(length % 50)
                 length -= length % 50
-                self.pas(length, self.getloop())
+                phases += self.pause(length)
+        return phases
 
-    def loopstart(self, count):
+    def subprogram_label(self, label: int):
+        '''
+        Writes fun prl to PPL as subprogram start label definition
+        Input label: int must be 0 to 99
+        '''
+        self.__phase()
+        self.file.write(f'fun prl {str(label).zfill(2)}\n')
+
+    def subprogram_select(self):
+        '''
+        Writes fun pri to PPL, subprogram selection input
+        '''
+        self.__phase()
+        self.file.write(f'fun pri')
+
+    def loopstart(self, count: int):
+        '''
+        Equivalent to PPL 'fun lps', the count is here instead of at the end (like in PPL)
+        '''
         self.loop.append(count)
         if len(self.loop) > 3:
             raise Exception("Up to three nested loops, you have too many")
-        self.file.write(f"\nphase\nfun lps\n")
+        self.__phase()
+        self.file.write(f"fun lps\n")
 
     def loopend(self):
+        '''
+        Equivalent to PPL 'fun lop <nn>', count is established at start self.loopstart(count)
+        '''
         self.file.write(f"\nphase\nfun lop {self.loop.pop()}\n")
 
     def getloop(self):
-        if len(self.loop) >= 1:
-            return self.loop[-1]
-        else:
-            return sympy.prod(self.loop)
+        '''
+        Returns how many times a given phase would output due to loops
+        '''
+        return sympy.prod(self.loop)
+    
+    def jump(self, phase: str):
+        '''
+        Writes to PPL 'fun jmp'
+        Not compatible with pumpz.sync() or self.time
+        Inputs:
+        ============================
+        self: pump object
+        phase: phase number OR phase name ( as set by self.label(label: str) )
+        '''
+        self.__phase()
+        self.sync_is_useable = False
+        self.file.write(f'fun jmp {self.__phase_to_string(phase)}')
+    
+    def if_low(self, phase):
+        '''
+        Equivalent to PPL 'fun if <nn>'
+        Inputs:
+        ============================
+        self: pump object
+        phase: phase number OR phase name ( as set by self.label(label: str) )
+        '''
+        self.sync_is_useable = False
+        self.__phase()
+        self.file.write(f'fun if {self.__phase_to_string(phase)}')
+    
+    def event_trap(self, phase):
+        '''
+        Equivalent to PPL 'fun evn <nn>'
+        Inputs:
+        ============================
+        self: pump object
+        phase: phase number OR phase name ( as set by self.label(label: str) )
+        '''
+        self.sync_is_useable = False
+        self.__phase()
+        self.file.write(f'fun evn {self.__phase_to_string(phase)}')
+    
+    def event_trap_sq(self,phase):
+        '''
+        Equivalent to PPL 'fun evs <nn>'
+        Inputs:
+        ============================
+        self: pump object
+        phase: phase number OR phase name ( as set by self.label(label: str) )
+        '''
+        self.sync_is_useable = False
+        self.__phase()
+        self.file.write(f'fun evs {self.__phase_to_string(phase)}')
+    
+    def event_reset(self):
+        '''
+        Equivalent to PPL 'fun evr'
+        '''
+        self.__phase()
+        self.file.write(f'fun evr')
+
+    # cld: clear total dispense volume, not implemented
+
+    def trg(self, num: int):
+        self.__phase()
+        self.file.write(f'fun trg {num}')
+
+    def out(self, n):
+        self.__phase()
+        self.file.write(f'fun out {int(n)}')
 
     def stop(*args):
         for self in args:
-            self.file.write(f"\nphase\nfun stp\n")
+            self.__phase()
+            self.file.write(f"fun stp\n")
 
     def sync(*args):
         max_time = 0
         for arg in args:
             if arg.time > max_time:
                 max_time = arg.time
+            if arg.sync_is_useable == False:
+                raise Exception(f'sync isn\'t useable with {arg}')
         for arg in args:
             time_diff = max_time - arg.time
             if time_diff > 0:
-                arg.pas(math.ceil(time_diff))
+                arg.pause(math.ceil(time_diff))
 
--- tests/test.py
+++ tests/test.py
@@ -1,30 +1,48 @@
-from pumpz import *
+import pumpz as pz
+import math
 
 f_aq = open("aq.ppl", "w")
 f_org = open("org.ppl", "w")
-f_wash = open("wash.ppl", "w")
-f_master = open('master.ppl','w')
+f_master = open("master.ppl", "w")
 
-aq = pump(f_aq, 26.59)
-org = pump(f_org, 26.59)
-wash = pump(f_wash, 26.59)
+aq = pz.pump(f_aq, 26.59, "mm", "mL")
+org = pz.pump(f_org, 26.59, "mm", "mL")
 
-master = masterppl(f_master)
-master.quickset({0:org,1:aq,2:wash})
+master = pz.masterppl(f_master)
+master.quickset({0: org, 1: aq})
 
-pump.init(aq,wash,org)
-aq.rat(22, 20, "wdr") 
-org.rat(22, 20, "wdr") 
-wash.rat(21,50,'wdr')
-aq.rat(10, 20, "inf") 
-aq.pas(5 * 60)
-pump.sync(aq, org)
-org.rat(10, 20, "inf") 
-org.pas(5 * 60)
-pump.sync(org,wash)
-wash.loopstart(2)
-wash.rat(21, 50, "inf")
-wash.rat(21, 50, "wdr")
-wash.loopend()
-wash.rat(21, 50, "inf")
-pump.stop(aq,org,wash)
+pz.pump.init(aq, org)
+aq.rate(22, 20, "wdr")
+org.rate(22, 20, "wdr")
+
+aq.rate(10, 20, "inf")
+aq.pause(5 * 60)
+pz.pump.sync(aq, org)
+
+org.rate(10, 20, "inf")
+t0 = math.ceil(org.time)
+org.pause(60)
+pz.pump.sync(aq, org)
+
+aq.rate(22, 50, "wdr")
+org.rate(22, 50, "wdr")
+t1 = math.ceil(org.time)
+pause_length = t0 + 500 - t1
+if pause_length < 0:
+    print("Error: timing is incompatible")
+org.pause(pause_length)
+pz.pump.sync(aq, org)
+
+aq.loopstart(2)
+org.loopstart(2)
+
+aq.rate(22, 50, "inf")
+org.rate(22, 50, "inf")
+aq.rate(22, 50, "wdr")
+org.rate(22, 50, "wdr")
+
+aq.loopend()
+org.loopend()
+
+aq.rate(22, 50, "inf")
+org.rate(22, 50, "inf")
