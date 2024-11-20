96abcdde64c05c2481e60406f9607be815a2e549
Faisal-Shahbaz <faisalsshahbaz1@gmail.com>
2024-11-20 13:04:26 -0500

    v0.2.0 released

--- src/pumpz/__init__.py
+++ src/pumpz/__init__.py
@@ -1,7 +1,6 @@
-# from .utilities import *
 from .masterppl import *
 from .pump import *
 
 def details():
-    print(f'Hello! You\'re using version 0.1.0 of the python library pumpz by Faisal Shahbaz')
-    return 1
+    print(f'Hello! You\'re using version 0.2.0 of the python library pumpz by Faisal Shahbaz')
+    return True
